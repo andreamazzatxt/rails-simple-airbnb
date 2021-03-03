6c14c73de974e86e80b375153397dab14ee10414
andreamazzatxt <andreamazza89@gmail.com>
2021-03-03 14:30:26 +0100

    rails new migrate db and seed controller and model created included bootstrap and simpleform

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Starting seed'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Beautiful Flat',
+  address: 'Barcelona',
+  description: 'Simply beautiful',
+  price_per_night: 115,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Space Flat',
+  address: '10 London W9 1DT',
+  description: 'Spacious Flat!!!',
+  price_per_night: 50,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Beatiful Villa with SeaView',
+  address: 'Tulum Mexico',
+  description: 'A lovely summer feel for this spacious Villa. ',
+  price_per_night: 160,
+  number_of_guests: 5
+)
+puts 'Seed Completed!!'
